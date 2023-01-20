78c1ffc0cc57832f739efe62ad02dec0c06747cf
unknown <absolem.dev@gmail.com>
2023-01-20 23:00:51 +0400

    add click-analytics module

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,32 @@
-import {Module} from '../core/module'
+import {Module} from '../core/module';
 
 export class ClicksModule extends Module {
+  constructor(type, text) {
+    super();
+    this.countClicks = 0;
+    this.countDoubleClicks = 0;
+  }
 
+  trigger() {
+    const modalWraper = document.createElement('div');
+    modalWraper.className = '.modal-wrapper';
+    const modal = document.createElement('div');
+    modal.className = 'modal';
+    modal.innerText = 'Считаем клики...';
+    modalWraper.append(modal);
+    modalWraper.style.display = 'flex';
+    const handleClick = (e) => {
+      (e.type === 'click') ? this.countClicks += 1 : this.countDoubleClicks += 1;
+    };
+    document.addEventListener('click', handleClick);
+    document.addEventListener('dblclick', handleClick);
+    setTimeout(() => {
+      document.removeEventListener('click', handleClick);
+      document.removeEventListener('dblclick', handleClick);
+      modal.innerText = `Количество кликов: ${this.countClicks} <br> Количество двойных кликов: ${this.countDoubleClicks}`;
+      setTimeout(() => {
+        modalWraper.style.display = 'none';
+      }, 3000);
+    }, 3000);
+  }
 }
