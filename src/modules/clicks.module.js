879ec9ed1f260a0467789f9a692e92c33b1ebe5f
unknown <absolem.dev@gmail.com>
2023-01-21 09:33:57 +0400

    fix constructor, add modal to DOM, reset count clicks

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -2,7 +2,7 @@ import {Module} from '../core/module';
 
 export class ClicksModule extends Module {
   constructor(type, text) {
-    super();
+    super(type, text);
     this.countClicks = 0;
     this.countDoubleClicks = 0;
   }
@@ -14,6 +14,7 @@ export class ClicksModule extends Module {
     modal.className = 'modal';
     modal.innerText = 'Считаем клики...';
     modalWraper.append(modal);
+    document.append(modalWraper);
     modalWraper.style.display = 'flex';
     const handleClick = (e) => {
       (e.type === 'click') ? this.countClicks += 1 : this.countDoubleClicks += 1;
@@ -26,6 +27,8 @@ export class ClicksModule extends Module {
       modal.innerText = `Количество кликов: ${this.countClicks} <br> Количество двойных кликов: ${this.countDoubleClicks}`;
       setTimeout(() => {
         modalWraper.style.display = 'none';
+        this.countClicks = 0;
+        this.countDoubleClicks = 0;
       }, 3000);
     }, 3000);
   }
